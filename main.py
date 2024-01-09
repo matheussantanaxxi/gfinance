2c6fe8f0ef050db5da73b8bfbf6b321adf3ff06f
Matheus Santana <43189453+matheussantanaxxi@users.noreply.github.com>
2024-01-08 22:21:06 -0300

    Início de estrutura
    Configuração de janela

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from tkinter import Tk
+from tkinter import *
 
-app = Tk()
+class Application:
+    def __init__(self):
+        self.App = Tk()
+        self.main_window()
+        self.App.mainloop()
 
-app.mainloop()
+    
+    def main_window(self):
+        self.App.title("Gfinance")
+        self.App.geometry("800x500")
+        self.App.maxsize(width=1000, height=600)
+        self.App.minsize(width=800, height=500)
+
+        
+    
+
+Application()
